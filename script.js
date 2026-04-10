c2bd796e3068985e540c96dbc6b295d59a2521e2
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-04-10 14:53:14 -0300

    reestruturei um pouco o código e criei o eventLoop

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// @type {HTMLCanvasElement}
 const canvas = document.getElementById('canvas1');
+// @type {CanvasRenderingContext2D}
 const ctx = canvas.getContext('2d');
 
 let circulo = {
@@ -6,36 +8,42 @@ let circulo = {
     x: canvas.width/2,
     y: canvas.height/2
 };
-
+let ponto = {
+    x: 0,
+    y: 0
+};
 function criar_circulo() {
     ctx.strokeStyle = "black";
     ctx.beginPath();
     ctx.arc(circulo.x, circulo.y, circulo.r, 0, Math.PI*2);
     ctx.stroke();
 }
-
-document.onmousemove = function(event) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = "black"; // mudar isso depois?
+function criar_ponto(ponto) {   
+    ctx.strokeStyle = "black";
     ctx.beginPath();
-    ctx.arc(event.clientX, event.clientY, 10, 0, Math.PI*2);
+    ctx.arc(ponto.x, ponto.y, 5, 0, Math.PI*2);
     ctx.stroke(); ctx.fill();
-    criar_circulo();
+    ctx.closePath();
 }
-
-let ponto = {
-    x: 0,
-    y: 0
-};
 document.addEventListener("mousemove", (e) => {
     ponto.x = e.clientX;
     ponto.y = e.clientY;
 });
-let distance = ((ponto.x - circulo.x)**2 + (ponto.y - circulo.y)**2)**0.5;
-if(distance <= circulo.r){
-    console.log("Tem interseccção no ponto ( " + ponto.x + ", " + ponto.y + " )");
+function desenhar(circulo, ponto) { // responsavel pelo desenho na tela
+    criar_circulo(circulo);
+    criar_ponto(ponto);
 }
-else{
-    console.log("Sem intersecção");
+function main(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    desenhar(circulo, ponto)
+    
+    let distance = ((ponto.x - circulo.x)**2 + (ponto.y - circulo.y)**2)**0.5;
+    if(distance <= circulo.r){
+        console.log("Tem interseccção no ponto ( " + ponto.x + ", " + ponto.y + " )");
+    }
+    else{
+        console.log("Sem intersecção");
+    }
+    requestAnimationFrame(main);
 }
-console.log(`Distancia: ${distance}`);
+requestAnimationFrame(main);
